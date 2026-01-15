15c58a6dd23b1359805233b776100f8b1b48e14f
Taha Sayyed <codetaha786@gmail.com>
2026-01-15 10:54:08 +0530

    feat: add number conversion examples showing NaN and numeric parsing

---
+++ 01_basics/04_conversion_operation.js
@@ -0,0 +1,16 @@
+let var1="Taha"
+let valueNumber1=Number(var1)
+console.log(valueNumber1);//NaN
+console.log(typeof valueNumber1);
+
+console.log("--------------------------------");
+
+let var2="12.2"
+valueNumber2=Number(var2)
+console.log(valueNumber2);
+console.log(typeof valueNumber2);
+
+console.log("--------------------------------");
+
+
+
