4684a68163b1c52e8a112e345b965a9a531a8d3c
Taha Sayyed <codetaha786@gmail.com>
2026-01-15 10:52:40 +0530

    feat: add string concatenation, type coercion, and prefix/postfix operator examples

---
+++ 01_basics/03_operations.js
@@ -0,0 +1,35 @@
+console.log(2+2)
+
+let str1="hello"
+let str2=" Taha"
+console.log(str1+str2);
+console.log("Taha "+"Sayyed");
+
+console.log("----------------------------------------");
+
+
+var1="1"+2+2
+console.log(var1);
+console.log(typeof var1);
+
+var2=1+2+"2"
+console.log(var2);
+console.log(typeof var2);
+
+console.log("----------------------------------------");
+
+console.log("Concept of Postfix and Prefix");
+let x=3
+const y=x++;
+console.log(`x:${x}, y:${y}`);
+
+let p=3
+const q=++p
+console.log(`p:${p}, q:${q}`);
+
+
+
+
+
+
+
