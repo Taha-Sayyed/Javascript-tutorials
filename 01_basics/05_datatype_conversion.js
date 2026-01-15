495a325d93470767e419085a6efcd7b92cfde2b4
Taha Sayyed <codetaha786@gmail.com>
2026-01-15 10:57:29 +0530

    feat: add comparison and equality examples highlighting type coercion and strict checks

---
+++ 01_basics/05_datatype_conversion.js
@@ -0,0 +1,15 @@
+console.log("2">1);
+console.log("2">"1");
+console.log("2"==1);
+
+console.log("---------------------------------------");
+
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0);
+
+console.log("---------------------------------------");
+//Strict check
+console.log("2"===0);
+console.log(3===3);
+console.log(null===3);
