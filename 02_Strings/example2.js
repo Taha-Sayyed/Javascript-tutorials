69ff984e64c17987ed0e5ddaf58ecd621085700c
Taha Sayyed <codetaha786@gmail.com>
2026-01-15 20:52:51 +0530

    style: add string immutability and String object method examples

---
+++ 02_Strings/example2.js
@@ -0,0 +1,15 @@
+let firstname="Taha"//Immutable
+console.log(`Firstname is ${firstname} and its type is ${typeof firstname}`);
+console.log(firstname.toUpperCase());
+console.log(firstname);
+console.log(`Character at ${2} is ${firstname.charAt(2)}`);
+
+
+console.log("\n");
+
+
+
+let secondname=new String("Sayyed");//Immutable
+console.log(`Secondname is ${secondname} and its type is String ${typeof secondname}`);
+console.log(secondname.toUpperCase());
+console.log(`Character at ${2} is ${secondname.charAt(2)}`);
