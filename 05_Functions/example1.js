a0c281f70b40c53df7185b34d6b1e6913c2e1589
Taha Sayyed <codetaha786@gmail.com>
2026-01-18 00:06:20 +0530

    docs: demonstrate rest parameter usage in JavaScript functions

---
+++ 05_Functions/example1.js
@@ -0,0 +1,10 @@
+// Rest parameter
+
+function myFun(a,b,...manyMoreArgs){
+    console.log("a",a);
+    console.log("b",b);
+    console.log("manymoreArgs: ",manyMoreArgs);
+}
+
+myFun("Taha","Sayyed","Engineer","Sayyed","Backend Developer")
+
