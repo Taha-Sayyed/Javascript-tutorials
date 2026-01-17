8e0312ee2a7bcf129a7cd18e3d96a2b02c077ded
Taha Sayyed <codetaha786@gmail.com>
2026-01-17 20:15:43 +0530

    docs: demonstrate Object.keys, Object.values, and Object.entries usage

---
+++ 04_objects/example5.js
@@ -0,0 +1,16 @@
+const obj={
+    name:"Taha",
+    age:22,
+    location:"Mumbai"
+}
+
+const keys=Object.keys(obj)
+console.log(keys);
+
+const values=Object.values(obj)
+console.log(values);
+
+const entries=Object.entries(obj)
+console.log(entries);
+
+
