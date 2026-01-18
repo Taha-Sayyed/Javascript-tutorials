0ac42094ca22654b09eb076a0245ab0eefd7d3af
Taha Sayyed <codetaha786@gmail.com>
2026-01-19 01:34:03 +0530

    docs: demonstrate function hoisting differences between declarations and expressions

---
+++ 05_Functions/example3.js
@@ -0,0 +1,19 @@
+
+console.log(addone(5));
+function addone(num){
+    return num+1;
+}
+
+
+
+//addtwo(2)
+//Variables declared with const are not usable before they’re initialized
+const addtwo=function(num){
+    console.log(num+2);
+    
+}
+addtwo(5)
+
+console.log(typeof addone);
+console.log(typeof addtwo);
+
