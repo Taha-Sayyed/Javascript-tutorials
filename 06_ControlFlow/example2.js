deb19ae11047bf5e67864a19bc41cb3e65300775
Taha Sayyed <codetaha786@gmail.com>
2026-01-20 00:55:39 +0530

    docs: demonstrate checking empty arrays and objects in JavaScript

---
+++ 06_ControlFlow/example2.js
@@ -0,0 +1,14 @@
+//Example-1: Check whether Array is empty
+const userEmail=[]
+if(userEmail.length==0){
+    console.log("Array is Empty");
+}
+
+console.log("------------------------------------------------------");
+
+//Example-2: To check whether Object is Empty
+const emptyobj={}
+
+if(Object.keys(emptyobj).length===0){
+    console.log("Object is Empty");
+}
