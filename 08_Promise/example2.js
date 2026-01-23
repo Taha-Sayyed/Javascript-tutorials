5ae2549b218f922ddd6e4896a964f8e903f996b5
Taha Sayyed <codetaha786@gmail.com>
2026-01-23 20:24:29 +0530

    docs: demonstrate promise chaining, error handling, and async/await consumption

---
+++ 08_Promise/example2.js
@@ -0,0 +1,26 @@
+const promiseOne=new Promise(function(resolve,reject){
+    setTimeout(()=>{
+        let error=true;
+        if(error===false){
+            resolve({
+                username:"Taha",
+                Education:"Btech in AIML"
+            })
+        }else{
+            reject("Error: Something Went Wrong")
+        }
+    },2000)
+})
+
+promiseOne.then((obj)=>{
+    return obj.username;
+}).then((objReturn)=>{
+    console.log(objReturn);  
+}).catch((err)=>{
+    console.log(err);
+}).finally(()=>{
+    console.log("Finally called");
+    
+})
+
+
